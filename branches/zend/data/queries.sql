ade18a77676f6ca14fb097ce7f8ac37b8b1b742a
elmo.peele <elmo.peele@66443452-224d-0410-89f6-5dc266234ae1>
2010-04-05 13:41:46 +0000

    zend/library/Connexions/Model.php     Allow a Model to have a complex primary key.  Require that the first key is     the primary key (possibly complex).
    zend/library/Connexions.php
        Add Connexions::in_array() that will preform a recursive in_array().
        Currently used to locate keys within a key set that MAY contain complex
        keys.
    
    zend/application/models/UserItem.php
        Identify the primary key as a complex key.
    
    zend/data/queries.sql
        Additional, more optimal queries.
    
    zend/tests/library/model_userItem.php
        Simplify.

--- branches/zend/data/queries.sql
+++ branches/zend/data/queries.sql
@@ -14,16 +14,229 @@ SELECT t.*,
 SELECT t.*,
        uti.userItemCount,
        uti.itemCount,
-       uti.userCount
+       uti.userCount,
+       uti.tagCount
  FROM tag AS t
      INNER JOIN (
          SELECT tagId,
                 COUNT(DISTINCT itemId, userId) AS userItemCount,
-                COUNT(DISTINCT itemId) AS itemCount,
-                COUNT(DISTINCT userId) AS userCount
+                COUNT(DISTINCT itemId)         AS itemCount,
+                COUNT(DISTINCT userId)         AS userCount,
+                COUNT(DISTINCT tagId)          AS tagCount
                  FROM userTagItem
                  GROUP BY tagId) uti
      ON t.tagId=uti.tagId
  ORDER BY uti.userItemCount DESC
  LIMIT 50;
 
+-- **************************************************************************
+-- **** Tag Queries
+-- ****
+-- User-related tags (0.02 seconds)
+SELECT t.*,
+       uti.userItemCount,
+       uti.itemCount,
+       uti.userCount,
+       uti.tagCount
+  FROM tag as t
+  	INNER JOIN (
+		SELECT tagId,
+                COUNT(DISTINCT itemId, userId) AS userItemCount,
+                COUNT(DISTINCT itemId)         AS itemCount,
+                COUNT(DISTINCT userId)         AS userCount,
+                COUNT(DISTINCT tagId)          AS tagCount
+			FROM userTagItem
+			WHERE userId IN (1, 2, 3, 4)
+			GROUP BY tagId) uti
+	ON t.tagId=uti.tagId
+ ORDER BY uti.userItemCount DESC
+ LIMIT 50;
+
+-- Item-related tags
+SELECT t.*,
+       uti.userItemCount,
+       uti.itemCount,
+       uti.userCount,
+       uti.tagCount
+  FROM tag as t
+  	INNER JOIN (
+		SELECT tagId,
+                COUNT(DISTINCT itemId, userId) AS userItemCount,
+                COUNT(DISTINCT itemId)         AS itemCount,
+                COUNT(DISTINCT userId)         AS userCount,
+                COUNT(DISTINCT tagId)          AS tagCount
+			FROM userTagItem
+			WHERE itemId IN (1, 2, 3, 4)
+			GROUP BY tagId) uti
+	ON t.tagId=uti.tagId
+ ORDER BY uti.userItemCount DESC
+ LIMIT 50;
+
+-- UserItem-related tags
+SELECT t.*,
+       uti.userItemCount,
+       uti.itemCount,
+       uti.userCount,
+       uti.tagCount
+  FROM tag as t
+  	INNER JOIN (
+		SELECT tagId,
+                COUNT(DISTINCT itemId, userId) AS userItemCount,
+                COUNT(DISTINCT itemId)         AS itemCount,
+                COUNT(DISTINCT userId)         AS userCount,
+                COUNT(DISTINCT tagId)          AS tagCount
+			FROM userTagItem
+			WHERE userId=1 AND itemId=1
+			GROUP BY tagId) uti
+	ON t.tagId=uti.tagId
+ ORDER BY uti.userItemCount DESC
+ LIMIT 50;
+
+-- **************************************************************************
+-- **** User Queries
+-- ****
+-- Tag-related users (0.01 seconds)
+SELECT u.*,
+       uti.userItemCount,
+       uti.itemCount,
+       uti.userCount,
+       uti.tagCount
+  FROM user as u
+  	INNER JOIN (
+		SELECT userId,
+                COUNT(DISTINCT itemId, userId) AS userItemCount,
+                COUNT(DISTINCT itemId)         AS itemCount,
+                COUNT(DISTINCT userId)         AS userCount,
+                COUNT(DISTINCT tagId)          AS tagCount
+			FROM userTagItem
+			WHERE tagId IN (1, 2, 3, 4)
+			GROUP BY userId) uti
+	ON u.userId=uti.userId
+ ORDER BY uti.userItemCount DESC
+ LIMIT 50;
+
+-- Item-related users (0.01 seconds)
+SELECT u.*,
+       uti.userItemCount,
+       uti.itemCount,
+       uti.userCount,
+       uti.tagCount
+  FROM user as u
+  	INNER JOIN (
+		SELECT userId,
+                COUNT(DISTINCT itemId, userId) AS userItemCount,
+                COUNT(DISTINCT itemId)         AS itemCount,
+                COUNT(DISTINCT userId)         AS userCount,
+                COUNT(DISTINCT tagId)          AS tagCount
+			FROM userTagItem
+			WHERE itemId IN (1, 2, 3, 4)
+			GROUP BY userId) uti
+	ON u.userId=uti.userId
+ ORDER BY uti.userItemCount DESC
+ LIMIT 50;
+
+-- UserItem-related users
+SELECT u.*,
+       uti.userItemCount,
+       uti.itemCount,
+       uti.userCount,
+       uti.tagCount
+  FROM user as u
+  	INNER JOIN (
+		SELECT userId,
+                COUNT(DISTINCT itemId, userId) AS userItemCount,
+                COUNT(DISTINCT itemId)         AS itemCount,
+                COUNT(DISTINCT userId)         AS userCount,
+                COUNT(DISTINCT tagId)          AS tagCount
+			FROM userTagItem
+			WHERE userId=1 OR itemId=1
+			GROUP BY userId) uti
+	ON u.userId=uti.userId
+ ORDER BY uti.userItemCount DESC
+ LIMIT 50;
+
+-- **************************************************************************
+-- **** Item Queries
+-- ****
+-- User-related items (0.02 seconds)
+SELECT i.*,
+       uti.userItemCount,
+       uti.itemCount,
+       uti.userCount,
+       uti.tagCount
+  FROM item as i
+  	INNER JOIN (
+		SELECT itemId,
+                COUNT(DISTINCT itemId, userId) AS userItemCount,
+                COUNT(DISTINCT itemId)         AS itemCount,
+                COUNT(DISTINCT userId)         AS userCount,
+                COUNT(DISTINCT tagId)          AS tagCount
+			FROM userTagItem
+			WHERE userId IN (1, 2, 3, 4)
+			GROUP BY itemId) uti
+	ON i.itemId=uti.itemId
+ ORDER BY uti.userItemCount DESC
+ LIMIT 50;
+
+-- Tag-related items
+SELECT i.*,
+       uti.userItemCount,
+       uti.itemCount,
+       uti.userCount,
+       uti.tagCount
+  FROM item as i
+  	INNER JOIN (
+		SELECT itemId,
+                COUNT(DISTINCT itemId, userId) AS userItemCount,
+                COUNT(DISTINCT itemId)         AS itemCount,
+                COUNT(DISTINCT userId)         AS userCount,
+                COUNT(DISTINCT tagId)          AS tagCount
+			FROM userTagItem
+			WHERE tagId IN (1, 2, 3, 4)
+			GROUP BY itemId) uti
+	ON i.itemId=uti.itemId
+ ORDER BY uti.userItemCount DESC
+ LIMIT 50;
+
+-- UserItem-related items
+SELECT i.*,
+       uti.userItemCount,
+       uti.itemCount,
+       uti.userCount,
+       uti.tagCount
+  FROM item as i
+  	INNER JOIN (
+		SELECT itemId,
+                COUNT(DISTINCT itemId, userId) AS userItemCount,
+                COUNT(DISTINCT itemId)         AS itemCount,
+                COUNT(DISTINCT userId)         AS userCount,
+                COUNT(DISTINCT tagId)          AS tagCount
+			FROM userTagItem
+			WHERE userId=1 OR itemId=1
+			GROUP BY itemId) uti
+	ON i.itemId=uti.itemId
+ ORDER BY uti.userItemCount DESC
+ LIMIT 50;
+
+-- **************************************************************************
+-- **** UserItem Queries
+-- ****
+-- Tag-related UserItems
+SELECT ui.*,
+       uti.userItemCount,
+       uti.itemCount,
+       uti.userCount,
+       uti.tagCount
+  FROM userItem as ui
+  	INNER JOIN (
+		SELECT userId,itemId,
+                COUNT(DISTINCT itemId, userId) AS userItemCount,
+                COUNT(DISTINCT itemId)         AS itemCount,
+                COUNT(DISTINCT userId)         AS userCount,
+                COUNT(DISTINCT tagId)          AS tagCount
+			FROM userTagItem
+			WHERE tagId IN (1, 2, 3, 4)
+			GROUP BY userId,itemId) uti
+	ON ui.userId=uti.userId AND ui.itemId=uti.itemId
+ ORDER BY uti.userItemCount DESC
+ LIMIT 50;
