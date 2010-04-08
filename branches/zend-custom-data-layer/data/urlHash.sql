38d12a734bcddc68753fc806f2471e49eac7c219
elmo.peele <elmo.peele@66443452-224d-0410-89f6-5dc266234ae1>
2010-04-08 14:31:24 +0000

    Bad branch

--- branches/zend-custom-data-layer/data/urlHash.sql
+++
@@ -1,5 +0,0 @@
--- ALTER TABLE item ADD COLUMN urlHash VARCHAR(64) NOT NULL default '';
--- ALTER TABLE item ADD KEY i_urlHash (urlHash);
--- ALTER TABLE item DROP KEY i_url;
--- ALTER TABLE item CHANGE url url TEXT NO NULL default '';
-UPDATE item SET urlHash=MD5(LOWER(url));
