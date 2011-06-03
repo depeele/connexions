eb7cbe064f8408873638a2c7905644eb5f8f46c1
Elmo.Peele <Elmo.Peele@66443452-224d-0410-89f6-5dc266234ae1>
2011-06-03 19:10:26 +0000

    Working on the firefox extension.

---
+++ browser-extensions/firefox/defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.connexions.boolpref", false);
+pref("extensions.connexions.intpref", 0);
+pref("extensions.connexions.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.5a2c8fee-5c7f-11dc-8314-0800200c9a66.description", "chrome://connexions/locale/overlay.properties");
