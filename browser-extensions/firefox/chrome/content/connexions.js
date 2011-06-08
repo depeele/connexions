292b425b927ae4717d63b54f93aeabe26951c609
Elmo.Peele <Elmo.Peele@66443452-224d-0410-89f6-5dc266234ae1>
2011-06-08 23:37:06 +0000

    Working on the sqlite browser db.

--- browser-extensions/firefox/chrome/content/connexions.js
+++ browser-extensions/firefox/chrome/content/connexions.js
@@ -1,183 +1,53 @@
-/*****************************************************************************
- * UI / utility functions
+/** @file
+ *
+ *  The primary connexions class (connexions).
  *
  */
-var gLog  = null;
+const CC                    = Components.classes;
+const CI                    = Components.interfaces;
+const CR                    = Components.results;
+const CU                    = Components.utils;
+const CONNEXIONS_BASE_URL   = "%URL%";
 
-function Connexions_log(msg, stackFrame)
-{
-    if (gLog === null)
-    {
-        if (console && console.log)
-        {
-            gLog = function(msg) {
-                console.log(msg);
-            };
-        }
-        else
-        {
-            var console =
-                Components
-                    .classes['@mozilla.org/consoleservice;1']
-                        .getService(Components.interfaces.nsIConsoleService);
-
-            gLog = function(msg) {
-                console.logStringMessage(msg);
-            };
-        }
-    }
-
-    if (stackFrame === undefined)
-    {
-        stackFrame = Components.stack.caller;
-    }
-
-    if (msg.length < 80)
-    {
-        msg += '                                                                               '.substr(0, 80 - msg.length);
-    }
-
-    msg += " - Source File: "+ stackFrame.filename;
-    if (stackFrame.lineNumber !== undefined)
-    {
-        msg += ', line '+   stackFrame.lineNumber;
-
-        if (stackFrame.columnNumber !== undefined)
-        {
-            msg += ', column '+ stackFrame.columnNumber;
-        }
-    }
-    gLog(msg);
-}
-
-Connexions_log('Connexions: running...');
+CU.import("resource://connexions/debug.js");
+CU.import("resource://connexions/db.js");
 
 /*****************************************************************************
  * UI / Main
  *
  */
-const CONNEXIONS_BASE_URL   = "%URL%";
-const gWM                   =
-        Components.classes['@mozilla.org/appshell/window-mediator;1']
-                    .getService(Components.interfaces.nsIWindowMediator);
+function Connexions()
+{
+    this.initialized = false;
+    this.init();
+}
 
-var   connexions = {
+Connexions.prototype = {
+    wm:             CC['@mozilla.org/appshell/window-mediator;1']
+                        .getService(CI.nsIWindowMediator),
     prefsWindow:    null,
-    initialized:    false,
     strings:        null,
-
-    log: function(fmt) {
-        var msg = fmt;
-        for (var idex = 1; idex < arguments.length; idex++)
-        {
-            //msg = msg.replace(/%s/, connexions.obj2str(arguments[idex]));
-            msg = msg.replace(/%s/, arguments[idex]);
-        }
-
-        Connexions_log(msg, Components.stack.caller);
-    },
-
-    type: function(obj) {
-        var type    = (obj === null ? 'null' : typeof(obj));
-        if (type === 'object')
-        {
-            // What TYPE of object
-            if (obj.length) type = 'array';
-            if (obj.exec)   type = 'regexp';
-            if (obj.now)    type = 'date';
-        }
-
-        return type;
-    },
-
-    obj2str: function(obj, depth, maxDepth) {
-        var str = "";
-        if (obj == null) {
-            str += "null";
-            return str;
-        }
-
-        var type    = connexions.type(obj);
-        depth    = depth || 0;
-        maxDepth = maxDepth || 5;
-
-        if (depth > maxDepth)
-        {
-            return "%Exceeded depth "+ maxDepth +"%";
-        }
-
-        /*
-        Connexions_log('obj2str: obj[ '+ obj +' ], type[ '+ type +' ], '
-                        +'depth[ '+ depth +' ], max[ '+ maxDepth +' ]');
-        // */
-        switch (type)
-        {
-        case 'boolean':
-            str += type +"[ "+ (obj ? 'true' : 'false') +" ]";
-            break;
-
-        case 'string':
-            str += type +'[ "'+ obj +'" ]';
-            break;
-
-        case 'function':
-            str += type;
-            break;
-
-        case 'number':
-        case 'date':
-        case 'regexp':
-            str += type +"[ "+ obj +" ]";
-            break;
-
-        case 'array':
-        case 'object':
-            var open    = (type === 'array' ? '[' : '{');
-            var close   = (type === 'array' ? ']' : '}');
-            var parts   = [];
-            var indent  = '                                                 '
-                            .substr(0, (depth + 1) * 2);
-
-            for (var prop in obj) {
-                if (obj.hasOwnProperty(prop))
-                {
-                    if (type === 'array')
-                    {
-                        str = connexions.obj2str(obj[prop], depth+1);
-                    }
-                    else
-                    {
-                        str = indent
-                            +'"'+ prop +'": '
-                            + connexions.obj2str(obj[prop], depth+1);
-                    }
-                    parts.push(str);
-                }
-            }
-            if (type === 'object')
-            {
-                type = String(obj);
-                type = type.replace(/^\s*\[\s*/, '')
-                           .replace(/\s*\]\s*$/, '')
-                           .replace(/\s+Object$/, '');
-            }
-            str = type + open + parts.join(', ') + close;
-        }
-
-        return str;
-    },
+    db:             null,
 
     init: function() {
-        // initialization code
-        connexions.strings     = document.getElementById("connexions-strings");
-        connexions.initialized = true;
+        this.initialized = true;
+        this.strings     = document.getElementById("connexions-strings");
+        this.db          = new Connexions_Db();
 
-        connexions.log('connexions::init(): completed');
+        cDebug.log('connexions::init(): completed');
+
+        cDebug.log('connexions::init(): bookmarks[ %s ], tags[ %s ]',
+                   this.db.getTotalBookmarks(),
+                   this.db.getTotalTags());
+
+        var bookmarks   = this.db.getBookmarks();
+        cDebug.log('connexions::init(): retrieved %s bookmarks',
+                   bookmarks.length);
     },
 
     getString: function(name) {
-        return (connexions.strings
-                    ? connexions.strings.getString(name)
+        return (this.strings
+                    ? this.strings.getString(name)
                     : null);
     },
 
@@ -195,13 +65,13 @@ var   connexions = {
                         ? document.URL
                         : gURLBar.value);
 
-        connexions.log('tagPage(): docUrl[ %s ]', docUrl);
+        cDebug.log('tagPage(): docUrl[ %s ]', docUrl);
 
         switch (type)
         {
         case 'page':
             /*
-            var browser     = connexions.getBrowser();
+            var browser     = this.getBrowser();
             var webNav      = browser.webNavigation;
             url  = (webNav.currentURI
                     ? webNav.currentURI.spec
@@ -216,14 +86,14 @@ var   connexions = {
                         ? document.title
                         : url);
 
-            var selection   = connexions.getSelectedText();
+            var selection   = this.getSelectedText();
             var description = (selection.str ? selection.str : '');
 
-            connexions.log('tagPage(): type[ %s ], url[ %s ], name[ %s ], '
+            cDebug.log('tagPage(): type[ %s ], url[ %s ], name[ %s ], '
                             +   'description[ %s ]',
                            type, url, name, description);
 
-            connexions.openTagPage(url, name, description);
+            this.openTagPage(url, name, description);
             break;
 
         case 'link':
@@ -234,14 +104,14 @@ var   connexions = {
             name = gContextMenu.linkText();
             // */
 
-            connexions.log('tagPage(): type[ %s ]', type);
+            cDebug.log('tagPage(): type[ %s ]', type);
 
             // el should NEVER be null here
             url  = el.getAttribute('href');
-            connexions.log('tagPage(): type[ %s ], url[ %s ]', type, url);
+            cDebug.log('tagPage(): type[ %s ], url[ %s ]', type, url);
 
             name = el.textContent;
-            connexions.log('tagPage(): type[ %s ], url[ %s ], name[ %s ]',
+            cDebug.log('tagPage(): type[ %s ], url[ %s ], name[ %s ]',
                             type, url, name);
 
         case 'media':
@@ -251,7 +121,7 @@ var   connexions = {
                 url  = gContextMenu.target.getAttribute('src');
                 name = gContextMenu.target.getAttribute('title');
                 // */
-                connexions.log('tagPage(): type[ %s ], UNDEFINED url...',
+                cDebug.log('tagPage(): type[ %s ], UNDEFINED url...',
                                 type);
 
                 // el should NEVER be null here
@@ -269,7 +139,7 @@ var   connexions = {
 
             if (! url.match(/^[^:]:\/\//))
             {
-                connexions.log('tagPage(): type[ %s ], prepend docUrl...',
+                cDebug.log('tagPage(): type[ %s ], prepend docUrl...',
                                 type);
 
                 /* prepend the document URL taking care not to duplicate '/'
@@ -296,45 +166,45 @@ var   connexions = {
                 }
             }
 
-            connexions.log('tagPage(): type[ %s ], final url[ %s ], name[ %s ]',
+            cDebug.log('tagPage(): type[ %s ], final url[ %s ], name[ %s ]',
                            type, url, name);
 
-            connexions.openTagPage(url, name);
-            //connexions.popupAlert(type);
+            this.openTagPage(url, name);
+            //this.popupAlert(type);
             break;
         }
     },
 
     popupAlert: function(msg) {
         var promptService =
-                Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                          .getService(Components.interfaces.nsIPromptService);
+                CC["@mozilla.org/embedcomp/prompt-service;1"]
+                          .getService(CI.nsIPromptService);
         promptService.alert(window, 'Connexions Alert', msg);
     },
 
     toolbarButtonCommand: function(e) {
         // just reuse the function above.    you can change this, obviously!
-        connexions.popupAlert('toolbar button');
+        this.popupAlert('toolbar button');
     },
 
     showOptions: function() {
-        connexions.log("showOptions()");
+        cDebug.log("showOptions()");
 
-        if (! connexions.prefsWindow || connexions.prefsWindow.closed) {
+        if (! this.prefsWindow || this.prefsWindow.closed) {
             var xul     = 'chrome://connexions/content/options.xul';
             var title   = 'Connexions Options';
             var opts    = 'chrome,titlebar,toolbar,centerscreen,dialog=no';
-            connexions.prefsWindow =
-                connexions.openXulWindow(xul, title, opts);
+            this.prefsWindow =
+                this.openXulWindow(xul, title, opts);
         }
-        connexions.prefsWindow.focus();
+        this.prefsWindow.focus();
     },
 
     loadPage: function(e, page) {
         var url = null;
 
-        connexions.log("loadPage(): event[ %s ], page[ %s ]",
-                        connexions.obj2str(e),
+        cDebug.log("loadPage(): event[ %s ], page[ %s ]",
+                        cDebug.obj2str(e),
                         page);
         switch (page)
         {
@@ -372,20 +242,20 @@ var   connexions = {
         }
 
         if (url !== null) {
-            connexions.browserLoadPage(e, connexions.url(url));
+            this.browserLoadPage(e, this.url(url));
         }
     },
 
     getBrowser: function() {
-        return (gWM.getMostRecentWindow('navigator:browser')).getBrowser();
+        return (this.wm.getMostRecentWindow('navigator:browser')).getBrowser();
         //document.getElementById('content');
     },
 
     browserLoadPage: function(e, url) {
-        var browser = connexions.getBrowser();
+        var browser = this.getBrowser();
 
         // Open the url in a new tab
-        connexions.openTab(url);
+        this.openTab(url);
     },
 
     openXulWindow: function(xul, title, options, url) {
@@ -393,7 +263,7 @@ var   connexions = {
     },
 
     openTab: function(url) {
-        var browser = connexions.getBrowser();
+        var browser = this.getBrowser();
         var tab     = browser.addTab(url);
         browser.selectedTab = tab;
 
@@ -415,22 +285,22 @@ var   connexions = {
                     + ',height='+ height;
 
         /*
-        connexions.log("openPopupWindow(): url    [ %s ]", url);
-        connexions.log("openPopupWindow(): title  [ %s ]", title);
-        connexions.log("openPopupWindow(): options[ %s ]", options);
-        connexions.log("openPopupWindow(): xul    [ %s ]", xul);
+        cDebug.log("openPopupWindow(): url    [ %s ]", url);
+        cDebug.log("openPopupWindow(): title  [ %s ]", title);
+        cDebug.log("openPopupWindow(): options[ %s ]", options);
+        cDebug.log("openPopupWindow(): xul    [ %s ]", xul);
         // */
 
         /*
         var xul         = 'chrome://browser/content/browser.xul';
-        var newWindow   = connexions.openXulWindow(xul, title, options, url);
+        var newWindow   = this.openXulWindow(xul, title, options, url);
         */
         //var newWindow   = window.open(url, '_parent', options);
         var newWindow   = window.open(url, '_blank', options);
 
         /*
         newWindow.addEventListener("close", function() {
-                                    connexions.log("openPopupWindow(): "
+                                    cDebug.log("openPopupWindow(): "
                                                    + "close event triggered");
 
                                     newWindow.close();
@@ -455,14 +325,14 @@ var   connexions = {
         query += '&noNav&closeAction=close';
 
         /*
-        connexions.log("openTagPage(): url[ %s ]", url);
-        connexions.log("openTagPage(): name[ %s ]", name);
-        connexions.log("openTagPage(): description[ %s ]", description);
-        connexions.log("openTagPage(): tags[ %s ]", tags);
-        connexions.log("openTagPage(): query[ %s ]", query);
+        cDebug.log("openTagPage(): url[ %s ]", url);
+        cDebug.log("openTagPage(): name[ %s ]", name);
+        cDebug.log("openTagPage(): description[ %s ]", description);
+        cDebug.log("openTagPage(): tags[ %s ]", tags);
+        cDebug.log("openTagPage(): query[ %s ]", query);
         // */
 
-        connexions.openPopupWindow( connexions.url('post'+ query),
+        this.openPopupWindow( this.url('post'+ query),
                                     'Bookmark a Page' );
     },
 
@@ -502,18 +372,20 @@ var   connexions = {
     }
 };
 
-/*
-Connexions_log('Connexions: types: number[ '+ connexions.type(5) +' ]');
-Connexions_log('Connexions: types: number[ '+ connexions.type(5.5) +' ]');
-Connexions_log('Connexions: types: bool  [ '+ connexions.type(true) +' ]');
-Connexions_log('Connexions: types: bool  [ '+ connexions.type(false) +' ]');
-Connexions_log('Connexions: types: array [ '+ connexions.type([1,2]) +' ]');
-Connexions_log('Connexions: types: regexp[ '+ connexions.type(/abc/) +' ]');
-Connexions_log('Connexions: types: object[ '+ connexions.type({a:1}) +' ]');
-Connexions_log('Connexions: types: null  [ '+ connexions.type(null)  +' ]');
-Connexions_log('Connexions: types: string[ '+ connexions.type("string") +' ]');
-// */
-Connexions_log('Connexions: types: object[ '+ connexions.obj2str({a:1}) +' ]');
+var connexions  = new Connexions();
 
-window.addEventListener("load",   connexions.init,    false);
-window.addEventListener("unload", connexions.destroy, false);
+/*
+Connexions_log('Connexions: types: number[ '+ cDebug.type(5) +' ]');
+Connexions_log('Connexions: types: number[ '+ cDebug.type(5.5) +' ]');
+Connexions_log('Connexions: types: bool  [ '+ cDebug.type(true) +' ]');
+Connexions_log('Connexions: types: bool  [ '+ cDebug.type(false) +' ]');
+Connexions_log('Connexions: types: array [ '+ cDebug.type([1,2]) +' ]');
+Connexions_log('Connexions: types: regexp[ '+ cDebug.type(/abc/) +' ]');
+Connexions_log('Connexions: types: object[ '+ cDebug.type({a:1}) +' ]');
+Connexions_log('Connexions: types: null  [ '+ cDebug.type(null)  +' ]');
+Connexions_log('Connexions: types: string[ '+ cDebug.type("string") +' ]');
+// */
+Connexions_log('Connexions: types: object[ '+ cDebug.obj2str({a:1}) +' ]');
+
+window.addEventListener("load",   function(){ connexions.init(); },    false);
+window.addEventListener("unload", function(){ connexions.destroy(); }, false);
