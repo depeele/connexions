23fab6dec46e660119f2942de5679c704ff227bf
Elmo.Peele <Elmo.Peele@66443452-224d-0410-89f6-5dc266234ae1>
2011-08-04 11:22:28 +0000

    Add USER

--- data/connexions-create.sh
+++ data/connexions-create.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
   DB='connexions'
+USER='connexions'
 PASS=$1
 
 echo "Dropping ${DB} database..."
@@ -9,7 +10,7 @@ echo "Creating ${DB} database..."
 mysqladmin --user=root --password="$1" create ${DB}
 
 echo "Grating access..."
-mysql --user=root --password="$1" mysql -e "GRANT ALL ON ${DB}.* TO '${DB}'@'localhost';"
+mysql --user=root --password="$1" mysql -e "GRANT ALL ON ${DB}.* TO '${USER}'@'localhost';"
 
 echo "Creating tag tables..."
-mysql -u ${DB} ${DB} < connexions.sql
+mysql -u ${USER} ${DB} < connexions.sql
