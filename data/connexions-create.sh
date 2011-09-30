7b7028e8cd07f281e076984a9c7818bf1d9e6776
Elmo.Peele <Elmo.Peele@66443452-224d-0410-89f6-5dc266234ae1>
2011-09-30 10:51:33 +0000

    Change to use mysql directly instead of relying on mysqladmin.

--- data/connexions-create.sh
+++ data/connexions-create.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
-  DB='connexions'
-USER='connexions'
-PASS=$1
+     DB='connexions'
+   USER='connexions'
+   PASS=$1
+charSet='utf8'
+collate='utf8_roman_ci'
 
 echo "Dropping ${DB} database..."
-mysqladmin --user=root --password="$1" drop ${DB}
+#mysqladmin --user=root --password="${PASS}" drop ${DB}
+mysql --user=root --password="${PASS}" -e \
+	"DROP DATABASE ${DB};"
 
-echo "Creating ${DB} database..."
-mysqladmin --user=root --password="$1" create ${DB}
+echo "Creating ${DB} database (charset ${charSet}, collation ${collate})..."
+#mysqladmin --user=root --password="${PASS}" create ${DB}
+mysql --user=root --password="${PASS}" -e \
+	"CREATE DATABASE ${DB} CHARACTER SET ${charSet} COLLATE ${collate};"
 
 echo "Grating access..."
-mysql --user=root --password="$1" mysql -e "GRANT ALL ON ${DB}.* TO '${USER}'@'localhost';"
+mysql --user=root --password="${PASS}" -e \
+	"GRANT ALL ON ${DB}.* TO '${USER}'@'localhost';"
 
 echo "Creating tag tables..."
 mysql -u ${USER} ${DB} < connexions.sql
