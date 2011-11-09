a5cf4fceedb9457a379ac15a51e32f11aacf6834
Elmo.Peele <Elmo.Peele@66443452-224d-0410-89f6-5dc266234ae1>
2011-11-09 15:21:20 +0000

    Update the bookmarklets to properly use the site's absolute URL.  Bump the build number.

--- branches/zend/application/views/scripts/help/bookmarklet-basic_redirect.php
+++ branches/zend/application/views/scripts/help/bookmarklet-basic_redirect.php
@@ -1,1 +1,1 @@
-javascript:(function(){location.href='<?= $this->baseUrl('post') ?>?url='+encodeURIComponent(location.href)+'&name='+encodeURIComponent(document.title)+'&noNav&closeAction=back';}())
+javascript:(function(){location.href='<?= $this->serverUrl('/post') ?>?url='+encodeURIComponent(location.href)+'&name='+encodeURIComponent(document.title)+'&noNav&closeAction=back';}())
